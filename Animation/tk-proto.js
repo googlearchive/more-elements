360606bff10a1572269ab21fa5d6f344899be288
Yvonne Yip <ykyyip@google.com>
2013-05-08 12:27:33 -0700

    animation: get targets before playing a group

---
+++ Animation/tk-proto.js
@@ -0,0 +1,15 @@
+var tkAnimationProto = function(name) {
+  return {
+    publish: {
+      animationName: name,
+      targetLabel: '(no target)'
+    },
+    targetChanged: function() {
+      this.super(),
+      this.targetLabel = this.target ? this.target.id : '(no target)';
+    },
+    clickHandler: function() {
+      setTimeout(this.play.bind(this));
+    }
+  };
+};
